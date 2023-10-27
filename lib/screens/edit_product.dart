34310bd1b6d75fe87dabf304f22be4d8bce738da
Hishamk99 <hishamkamel370@gmail.com>
2023-10-27 14:35:14 +0200

    future builder to create EditScreen

--- lib/screens/edit_product.dart
+++ lib/screens/edit_product.dart
@@ -1,10 +1,37 @@
+import 'package:e_commerce/models/product_model.dart';
+import 'package:e_commerce/services/store.dart';
 import 'package:flutter/material.dart';
 
-class EditProduct extends StatelessWidget {
+class EditProduct extends StatefulWidget {
   const EditProduct({super.key});
   static String id = 'EditProduct';
+
+  @override
+  State<EditProduct> createState() => _EditProductState();
+}
+
+class _EditProductState extends State<EditProduct> {
+  Store store = Store();
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold());
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder<List<ProductModel>>(
+          future: store.loadProduct(),
+          builder: (context, snapshot) {
+           if(snapshot.hasData){ 
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Text(snapshot.data![index].name);
+              },
+            );
+            }
+            return const Text('Loading...');
+          },
+        ),
+      ),
+    );
   }
 }
